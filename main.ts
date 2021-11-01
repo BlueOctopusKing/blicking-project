d31f1cdc721a5a0f12eeb0f434be0aaa1009c0ba
BlueOctopusKing <92817520+BlueOctopusKing@users.noreply.github.com>
2021-11-01 13:36:13 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,106 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            . . . . .
+            # . # . #
+            . # . # .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . . . . .
+            . # . # .
+            # . # . #
+            `)
+    }
+    basic.showLeds(`
+        # . # . #
+        # # . # .
+        # . . . .
+        # . # . #
+        # # . # .
+        `)
+    basic.showLeds(`
+        # # . # .
+        # # # . #
+        # # . . .
+        # # . # .
+        # # # . #
+        `)
+    basic.showLeds(`
+        # # # . #
+        # # # # .
+        # # # . .
+        # # # . #
+        # # # # .
+        `)
+    basic.showLeds(`
+        # # # # .
+        # # # # #
+        # # # # .
+        # # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showString("DO YOUR HOMEWORK")
+})
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        . . . . .
+        # . # . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . . . . .
+        . # . # .
+        # . # . #
+        `)
+}
+basic.showLeds(`
+    # . # . #
+    # # . # .
+    # . . . .
+    # . # . #
+    # # . # .
+    `)
+basic.showLeds(`
+    # # . # .
+    # # # . #
+    # # . . .
+    # # . # .
+    # # # . #
+    `)
+basic.showLeds(`
+    # # # . #
+    # # # # .
+    # # # . .
+    # # # . #
+    # # # # .
+    `)
+basic.showLeds(`
+    # # # # .
+    # # # # #
+    # # # # .
+    # # # # .
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.showString("DO YOUR HOMEWORK")
